4f74ba7a56133c53c311976c3a7359d26e91f214
AaronPengwp <weiping_551@163.com>
2019-05-21 16:44:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+随机请求头与IP代理池的使用
+httpbin.py：随机请求头代码
+ipproxy.py：IP代理池代码
+middlewares.py：主要设置代码
---
+++ start.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# --*-- coding:utf-8 --*--
+# __Author__ Aaron
+
+from scrapy import cmdline
+
+# cmdline.execute("scrapy crawl httpbin".split())
+cmdline.execute("scrapy crawl ipproxy".split())
